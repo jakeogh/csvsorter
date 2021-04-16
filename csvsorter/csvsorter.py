48ae9b21776c88e05c92c5191c055a8cad2ec4ad
jakeogh <github.com@v6y.net>
2021-04-15 20:30:54 -0700

    auto-commit

--- csvsorter/csvsorter.py
+++ csvsorter/csvsorter.py
@@ -19,8 +19,13 @@
 # pylint: disable=R0916  # Too many boolean expressions in if statement
 
 
+import csv
+import heapq
+import logging
+import multiprocessing
 import os
 import sys
+import tempfile
 from pathlib import Path
 from typing import ByteString
 from typing import Generator
@@ -34,6 +39,13 @@ from enumerate_input import enumerate_input
 #from with_sshfs import sshfs
 #from with_chdir import chdir
 from retry_on_exception import retry_on_exception
+
+csv.field_size_limit(2**30)  # can't use sys.maxsize because of Windows error
+
+
+class CsvSortError(Exception):
+    pass
+
 
 #from prettytable import PrettyTable
 #output_table = PrettyTable()
@@ -56,135 +68,255 @@ except ImportError:
 # #set_trace(term_size=(80, 24))
 # from pudb import set_trace; set_trace(paused=False)
 
+
+
+def _get_reader(input_file, csv_reader, encoding, delimiter):
+    """Get the reader instance. This will either open the file, or
+    return the csv_reader supplied by the caller.
+    """
+    if csv_reader:
+        return csv_reader
+
+    with open(input_file, newline='', encoding=encoding) as input_fp:
+        return csv.reader(input_fp, delimiter=delimiter)
+
+
+def csvsort(input_file,
+            columns,
+            output_file,
+            max_size=100,
+            has_header=True,
+            delimiter=',',
+            show_progress=False,
+            parallel=True,
+            quoting=csv.QUOTE_MINIMAL,
+            encoding=None,
+            numeric_column=False,
+            csv_reader=None,
+            ):
+    """Sort the CSV file on disk rather than in memory.
+
+    The merge sort algorithm is used to break the file into smaller sub files
+
+    Args:
+        input_file: the CSV filename to sort.
+        columns: a list of columns to sort on (can be 0 based indices or header
+            keys).
+        output_file: filename for sorted file.
+        max_size: the maximum size (in MB) of CSV file to load in memory at once.
+        has_header: whether the CSV contains a header to keep separated from sorting.
+        delimiter: character used to separate fields, default ','.
+        show_progress: A flag whether or not to show progress.
+            The default is False, which does not print any merge information.
+        quoting: How much quoting is needed in the final CSV file.  Default is
+            csv.QUOTE_MINIMAL.
+        encoding: The name of the encoding to use when opening or writing the
+            csv files. Default is None which uses the system default.
+        numeric_column: If columns being used for sorting are all numeric and
+            the desired output is to have the sorting be done numerically rather
+            than string based. Default, False, does string-based sorting
+        csv_reader: a pre-loaded instance of `csv.reader`. This allows you to
+            supply a compatible stream for use in sorting.
+    """
+
+    reader = _get_reader(input_file,
+                         csv_reader=csv_reader,
+                         encoding=encoding,
+                         delimiter=delimiter,)
+    if has_header:
+        header = next(reader)
+    else:
+        header = None
+
+    columns = parse_columns(columns, header)
+
+    filenames = csvsplit(reader, max_size)
+    if show_progress:
+        logging.info('Merging %d splits' % len(filenames))
+
+    if parallel:
+        concurrency = multiprocessing.cpu_count()
+        with multiprocessing.Pool(processes=concurrency) as pool:
+            map_args = [(filename, columns, numeric_column, encoding)
+                        for filename in filenames]
+            pool.starmap(memorysort, map_args)
+    else:
+        for filename in filenames:
+            memorysort(filename, columns, numeric_column, encoding)
+    sorted_filename = mergesort(filenames,
+                                columns,
+                                numeric_column,
+                                encoding=encoding)
+
+    # XXX make more efficient by passing quoting, delimiter, and moving result
+    # generate the final output file
+    with open(output_file,
+              'w',
+              newline='',
+              encoding=encoding) as output_fp:
+        writer = csv.writer(output_fp, delimiter=delimiter, quoting=quoting)
+        if header:
+            writer.writerow(header)
+        with open(sorted_filename, newline='', encoding=encoding) as sorted_fp:
+            for row in csv.reader(sorted_fp):
+                writer.writerow(row)
+
+    os.remove(sorted_filename)
+
+
+def parse_columns(columns, header):
+    """check the provided column headers"""
+    for i, column in enumerate(columns):
+        if isinstance(column, int):
+            if header:
+                if column >= len(header):
+                    raise CsvSortError(
+                        'Column index is out of range: "{}"'.format(column))
+        else:
+            # find index of column from header
+            if header is None:
+                raise CsvSortError(
+                    'CSV needs a header to find index of this column name:' +
+                    ' "{}"'.format(column))
+            else:
+                if column in header:
+                    columns[i] = header.index(column)
+                else:
+                    raise CsvSortError(
+                        'Column name is not in header: "{}"'.format(column))
+    return columns
+
+
+def csvsplit(reader, max_size):
+    """Split into smaller CSV files of maximum size and return the filenames."""
+    max_size = max_size * 1024 * 1024  # convert to bytes
+    writer = None
+    current_size = 0
+    split_filenames = []
+
+    # break CSV file into smaller merge files
+    for row in reader:
+        if writer is None:
+            ntf = tempfile.NamedTemporaryFile(delete=False, mode='w')
+            writer = csv.writer(ntf)
+            split_filenames.append(ntf.name)
+
+        writer.writerow(row)
+        current_size += sys.getsizeof(row)
+        if current_size > max_size:
+            writer = None
+            current_size = 0
+    return split_filenames
+
+
+def memorysort(filename, columns, numeric_column, encoding=None):
+    """Sort this CSV file in memory on the given columns"""
+    with open(filename, newline='', encoding=encoding) as input_fp:
+        rows = [row for row in csv.reader(input_fp) if row]
+
+    rows.sort(key=lambda row: get_key(row, columns, numeric_column))
+    with open(filename, 'w', newline='', encoding=encoding) as output_fp:
+        writer = csv.writer(output_fp)
+        for row in rows:
+            writer.writerow(row)
+
+
+def get_key(row, columns, numeric_column):
+    """Get sort key for this row"""
+    if (numeric_column):
+        return [float(row[column]) for column in columns]
+    else:
+        return [row[column] for column in columns]
+
+
+def decorated_csv(filename, columns, numeric_column, encoding=None):
+    """Iterator to sort CSV rows
+    """
+    with open(filename, newline='', encoding=encoding) as fp:
+        for row in csv.reader(fp):
+            yield get_key(row, columns, numeric_column), row
+
+
+def mergesort(sorted_filenames,
+              columns,
+              numeric_column,
+              nway=2,
+              encoding=None):
+    """Merge these 2 sorted csv files into a single output file
+    """
+    merge_n = 0
+    while len(sorted_filenames) > 1:
+        merge_filenames, sorted_filenames = \
+            sorted_filenames[:nway], sorted_filenames[nway:]
+
+        with tempfile.NamedTemporaryFile(delete=False, mode='w') as output_fp:
+            writer = csv.writer(output_fp)
+            merge_n += 1
+            for _, row in heapq.merge(*[
+                    decorated_csv(filename, columns, numeric_column, encoding)
+                    for filename in merge_filenames
+            ]):
+                writer.writerow(row)
+
+            sorted_filenames.append(output_fp.name)
+
+        for filename in merge_filenames:
+            os.remove(filename)
+    return sorted_filenames[0]
+
+
 @click.command()
-@click.argument("paths", type=str, nargs=-1)
+@click.argument("input_file",
+                type=click.Path(exists=True,
+                                dir_okay=False,
+                                file_okay=True,
+                                path_type=str,
+                                allow_dash=False,),
+                nargs=1,
+                required=True,)
+@click.argument("output_file",
+                type=click.Path(exists=False,
+                                dir_okay=False,
+                                file_okay=True,
+                                path_type=str,
+                                allow_dash=True,),
+                nargs=1,
+                required=False,)
+@click.option('--column', 'columns', type=int, multiple=True, help='column(s) of CSV to sort on')
+@click.option('--max-size', type=int, default=100, help='maximum size of each split CSV file in MB (default 100)')
+@click.option('--no-header', help='set CSV file has no header')
+@click.option('--delimiter', type=str, default=',', help='set CSV delimiter (default ",")')
+@click.option('--encoding', type=str, help='character encoding (eg utf-8) to use when reading/writing files (default uses system default)')
 @click.option('--verbose', is_flag=True)
 @click.option('--debug', is_flag=True)
 @click.option("--printn", is_flag=True)
 @click.pass_context
 def cli(ctx,
-        paths,
-        sysskel,
+        input_file: str,
+        output_file: str,
+        columns,
+        max_size: int,
+        no_header: bool,
+        delimiter: str,
+        encoding: str,
         verbose: bool,
         debug: bool,
-        simulate: bool,
-        ipython: bool,
-        count: bool,
-        skip: int,
-        head: int,
-        tail: int,
         printn: bool,
         ):
 
-    null = not printn
-    end = '\n'
-    if null:
-        end = '\x00'
-    if sys.stdout.isatty():
-        end = '\n'
-        assert not ipython
+    has_header = not no_header
 
-    #progress = False
-    #if (verbose or debug):
-    #    progress = False
+    input_file = Path(input_file)
+    output_file = Path(output_file)
 
-    ctx.ensure_object(dict)
-    if verbose:
-        ctx.obj['verbose'] = verbose
-    verbose = ctx.obj['verbose']
-    if debug:
-        ctx.obj['debug'] = debug
-    debug = ctx.obj['debug']
-
-    ctx.obj['end'] = end
-    ctx.obj['null'] = null
-    #ctx.obj['progress'] = progress
-    ctx.obj['count'] = count
-    ctx.obj['skip'] = skip
-    ctx.obj['head'] = head
-    ctx.obj['tail'] = tail
-
-    #global APP_NAME
-    #config, config_mtime = click_read_config(click_instance=click,
-    #                                         app_name=APP_NAME,
-    #                                         verbose=verbose,
-    #                                         debug=debug,)
-    #if verbose:
-    #    ic(config, config_mtime)
-
-    #if add:
-    #    section = "test_section"
-    #    key = "test_key"
-    #    value = "test_value"
-    #    config, config_mtime = click_write_config_entry(click_instance=click,
-    #                                                    app_name=APP_NAME,
-    #                                                    section=section,
-    #                                                    key=key,
-    #                                                    value=value,
-    #                                                    verbose=verbose,
-    #                                                    debug=debug,)
-    #    if verbose:
-    #        ic(config)
-
-    iterator = paths
-
-    for index, path in enumerate_input(iterator=iterator,
-                                       null=null,
-                                       progress=False,
-                                       skip=skip,
-                                       head=head,
-                                       tail=tail,
-                                       debug=debug,
-                                       verbose=verbose,):
-        path = Path(path)
-
-        if verbose:  # or simulate:
-            ic(index, path)
-        #if count:
-        #    if count > (index + 1):
-        #        ic(count)
-        #        sys.exit(0)
-
-        #if simulate:
-        #    continue
-
-        with open(path, 'rb') as fh:
-            path_bytes_data = fh.read()
-
-        if not count:
-            print(path, end=end)
-
-    if count:
-        print(index + 1, end=end)
+    csvsort(input_file=input_file,
+            output_file=output_file,
+            columns=columns,
+            max_size=max_size,
+            has_header=has_header,
+            delimiter=delimiter,
+            encoding=encoding)
 
 #        if ipython:
 #            import IPython; IPython.embed()
-
-#@cli.command()
-#@click.argument("urls", type=str, nargs=-1)
-#@click.option('--verbose', is_flag=True)
-#@click.option('--debug', is_flag=True)
-#@click.pass_context
-#def some_command(ctx,
-#                 urls,
-#                 verbose: bool,
-#                 debug: bool,
-#                 ):
-#    pass
-#    verbose = verbose or ctx.obj['verbose']
-#    debug = debug or ctx.obj['debug']
-#
-#    iterator = urls
-#    for index, url in enumerate_input(iterator=iterator,
-#                                      null=ctx.obj['null'],
-#                                      progress=ctx.obj['progress'],
-#                                      skip=ctx.obj['skip'],
-#                                      head=ctx.obj['head'],
-#                                      tail=ctx.obj['tail'],
-#                                      debug=ctx.obj['debug'],
-#                                      verbose=ctx.obj['verbose'],):
-#
-#        if ctx.obj['verbose']:
-#            ic(index, url)
-
 
